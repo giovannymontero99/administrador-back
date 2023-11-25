e55405d4aa49b1a8730cb84b5d6b2d7be0dc0798
Giovanny Montero <giovanny.montero@sgsas.co>
2023-11-25 17:39:00 -0500

    New login route

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import router from "../v1/routes/route.js";
 
@@ -10,12 +11,17 @@ const app = express();
 
 // middlewares
 app.use(logger("dev"));
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173', // Reemplazar con el origen de tu frontend
+    credentials: true // Permite enviar cookies desde el frontend
+}));
 
 
 //configuraciones para recibir JSON
 app.use(express.json());
 
-app.use(cors());
+
 
 // routes
 app.use("/", router )
--- src/v1/routes/route.js
+++ src/v1/routes/route.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
 import  { productosController } from "../controller/controller.js";
+
+
+ //
+import jsonwebtoken from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs'
+import { usersCollection } from "../config/conection.database.js";
+import { secret } from "../config/variablesEntorno.js";
+
+///////
+
 const router = Router();
+
 
 
 
@@ -12,5 +23,41 @@ router.post("/",async(req,res)=>{
 router.get('/all-products', async(req, res) => {
     productosController.getAllData(req, res);
 });
+
+
+router.post('/login', async(req,res)=>{
+    const token = jsonwebtoken.sign( req.body, secret  );
+    const { userLoginForm, passwordLoginForm } = req.body;
+
+    const { password } = await usersCollection.findOne({
+        user : userLoginForm
+    });
+
+    const isRealPassword = await bcryptjs.compare( passwordLoginForm, password  );
+
+    if(isRealPassword) {
+        res.cookie( 'key', token, {
+            httpOnly: true,
+            sameSite: true,
+            secure: true
+        } )
+        res.send({ msg: 'OK' });
+    }else {
+        res.send({ msg: 'NOT AUTHORIZED' });
+    }
+})
+
+router.get('/ruta-protegida', (req,res)=>{
+    const token = req.cookies.key;
+    if (token) {
+        // El token es válido, puedes realizar acciones protegidas aquí
+        res.json({ mensaje: true });
+    } else {
+        // Sin token o token inválido, denegar el acceso
+        res.json({ mensaje: false});
+    }
+})
+
+
 
 export default router;
